b6e085dd486cddba31f884aa78c8baa387f4b1ab
Shivam <apple@shivam-MacBook-Air.local>
2022-07-29 17:44:13 +0530

    Updates

---
+++ precache-manifest.a3421ada08318bd57361d6bd8a73d54e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee99ed87ccfac9671729",
+    "url": "/Defi_Staking_App/static/css/main.91afecf0.chunk.css"
+  },
+  {
+    "revision": "ee99ed87ccfac9671729",
+    "url": "/Defi_Staking_App/static/js/main.ee99ed87.chunk.js"
+  },
+  {
+    "revision": "0372bad94481432c14c2",
+    "url": "/Defi_Staking_App/static/css/1.f96b9c76.chunk.css"
+  },
+  {
+    "revision": "0372bad94481432c14c2",
+    "url": "/Defi_Staking_App/static/js/1.0372bad9.chunk.js"
+  },
+  {
+    "revision": "8ea2a580f10783256bd1",
+    "url": "/Defi_Staking_App/static/js/runtime~main.8ea2a580.js"
+  },
+  {
+    "revision": "da70255120845f3066c5bdcc7367f259",
+    "url": "/Defi_Staking_App/static/media/tether.da702551.png"
+  },
+  {
+    "revision": "d5e1dbb21d95d186690d39b0b1811e7f",
+    "url": "/Defi_Staking_App/index.html"
+  }
+];
